5efc69c07f4150998e5e1a5ca699f334840899c6
kristen-justice <kjustice@uccs.edu>
2013-02-18 11:54:55 -0800

    Modified the model

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,7 +1,10 @@
 class Movie < ActiveRecord::Base
-  def Movie.get_all_ratings
-    all_ratings = []
-    self.select(:rating).group(:rating).each do |movie|
-      all_ratings << movie.rating
+#  def Movie.get_all_ratings
+#    all_ratings = []
+#    self.select(:rating).group(:rating).each do |movie|
+#      all_ratings << movie.rating
+#  end
+  def self.get_all_ratings
+    @@all_ratings = ["G", "PG", "PG-13", "R"]
   end
 end
