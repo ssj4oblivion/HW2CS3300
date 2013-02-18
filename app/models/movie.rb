120cab4aec65c35fb7fc15446b996776ad9841a8
Joel Judd <jjudd2@uccs.edu>
2013-02-18 10:30:38 -0700

    forgot a file

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,7 @@
 class Movie < ActiveRecord::Base
+  def Movie.get_all_ratings
+    all_ratings = []
+    self.select(:rating).group(:rating).each do |movie|
+      all_ratings << movie.rating
+  end
 end
