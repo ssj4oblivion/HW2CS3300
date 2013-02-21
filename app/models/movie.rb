d81a9a76613128cc40a6164f9158cb158900cacd
Joel Judd <jjudd2@uccs.edu>
2013-02-21 13:20:47 -0700

    dem typos

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -4,4 +4,10 @@ class Movie < ActiveRecord::Base
     self.select(:rating).group(:rating).each do |movie|
       all_ratings << movie.rating
   end
+  
+  #I was missiing this part
+  def sefl.get_all_ratings
+    @@all_ratings = ["G","PG","PG-13","R"]
+  end
+
 end
